c52ca064753746fc9dbe5698b700ad5c48d2f4c6
Michal Magula <m.magula23@gmail.com>
2023-05-12 21:15:04 +0200

    Added aritmetic filter

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include "BluetoothSerial.h"
 
 #define DEVICE_NAME "BikeEyes"
-#define DEVICE "ESP32_Devkit"
-#define LED_BUILTIN 2
+#define BUFF_SIZE 5
 
 char col; // For storing the data read from serial port
-unsigned char buffer[8] = {};
+unsigned char buff[8] = {};
+
+float dst_buff[BUFF_SIZE] = {};
+int curr_buff_idx = 0;
+
 int firstHalf = 0, secondHalf = 0;
 
 int dst = 0; // Distance in centimeters
@@ -18,34 +21,42 @@ float speed = 0; // Speed in m/s
 BluetoothSerial SerialBT;
 
 void sendData(float *dst, float *speed);
-void blink(int pinNumber, int delayTime = 1000, int repeatTimes = 3);
+float get_dst(float *d);
+float get_avg_dst();
 
 void setup()
 {
         Serial.begin(9600);
         Serial2.begin(57600); // Serial is used to connect to a Radar
         SerialBT.begin(DEVICE_NAME);
-        Serial.println("BikeEyes radar started");
-        blink(LED_BUILTIN, 1000, 3);
+        Serial.println("Started...");
+
+        // Init buffer with zeros
+        for (int i = 0; i < BUFF_SIZE; i++)
+        {
+                dst_buff[i] = 0;
+        }
 }
 
 void loop()
 {
+        // Send data only when received data
         if (Serial2.read() == 0xff)
         {
+                // Read the incoming byte.
                 for (int j = 0; j < 8; j++)
                 {
                         col = Serial2.read();
-                        buffer[j] = (char)col;
+                        buff[j] = (char)col;
                         delay(2);
                 }
                 Serial2.flush();
-                if (buffer[1] == 0xff)
+                if (buff[1] == 0xff)
                 {
-                        if (buffer[2] == 0xff)
+                        if (buff[2] == 0xff)
                         {
-                                firstHalf = buffer[3];
-                                secondHalf = buffer[4];
+                                firstHalf = buff[3];
+                                secondHalf = buff[4];
 
                                 last_dst = dst;
                                 dst = (firstHalf << 8) + secondHalf;
@@ -62,27 +73,40 @@ void loop()
 
                 // convert to metres and kilometres per hour
 
-                float dst_meters = (float)dst / 100;
+                float dst_meters = get_dst() / 100;
                 float vel_kmh = (float)speed * 3.6;
 
                 sendData(&dst_meters, &vel_kmh);
         }
 }
 
-void sendData(float *dst, float *speed)
+void sendData(float *d, float *spd)
 {
-        String dataString = String(*dst) + "," + String(*speed);
+        String dataString = String(*d) + "," + String(*spd);
         Serial.println(dataString);
         SerialBT.println(dataString);
 }
 
-void blink(int pinNumber, int delayTime, int repeatTimes)
+float get_dst()
 {
-        for (int i = 0; i < repeatTimes; i++)
+        dst_buff[curr_buff_idx] = dst;
+
+        curr_buff_idx++;
+        if (curr_buff_idx >= BUFF_SIZE)
         {
-                digitalWrite(pinNumber, HIGH);
-                delay(delayTime);
-                digitalWrite(pinNumber, LOW);
-                delay(delayTime);
+                curr_buff_idx = 0;
         }
+
+        return get_avg_dst();
+}
+
+float get_avg_dst()
+{
+        float total_dst = 0;
+        for (int i = 0; i < BUFF_SIZE; i++)
+        {
+                total_dst += dst_buff[i];
+        }
+
+        return total_dst / BUFF_SIZE;
 }
